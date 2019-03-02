d83aae8b3813c7d7c289db5fef66be479e7cf6ba
Steve Elliott <steve@tegud.net>
2019-03-02 16:59:55 +0000

    fix author and tags

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -5,13 +5,23 @@ import { Tags } from '@tryghost/helpers-gatsby'
 import _ from 'lodash'
 
 const CardTags = ({ post }) => {
-  const { tags } = post;
+  const { tags, id } = post;
 
   if (!tags.length) {
     return <span />;
   }
 
-  return <span>on <Tags post={post} visibility="public" autolink={false} /></span>;
+  // return <span>on <Tags post={post} visibility="public" autolink={false} /></span>;
+  return <span> on {tags.map(({ name, slug }, i) => {
+      const key = `${id}-tag-${slug}`;
+      const link = `/tag/${slug}`;
+      const separator = i+1 !== tags.length ? ', ' : <></>;
+
+      return <>
+        <Link key={key} to={link}>{name}</Link>{separator}
+      </>;
+    })}
+  </span>;
 }
 
 const PostCard = ({ post }) => {
