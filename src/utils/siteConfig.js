86bd02c813a1dda713f3aa46b8bd88baf39acc5c
Steve Elliott <steve@tegud.net>
2019-03-03 20:37:33 +0000

    and again...

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -10,5 +10,5 @@ module.exports = {
     shortTitle: `Tegud.net`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#5b618e`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    themeColor: `#5b618e`, // Used for Offline Manifest
 }
