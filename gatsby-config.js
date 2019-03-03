ab0d40fbd1aa961fbf28b0b16c8adb62a65f3888
Steve Elliott <steve@tegud.net>
2019-03-03 20:23:21 +0000

    turn offline back on

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,6 +179,7 @@ module.exports = {
         },
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
+        `gatsby-plugin-offline`,
         {
           resolve: `gatsby-plugin-google-analytics`,
           options: {
