d03d1ef9adfc57edc536ebc5a1180b965fded103
vndangky <dinhvinh1710@gmail.com>
2019-06-04 04:12:41 +0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -7,7 +7,7 @@ module.exports = {
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `W88.com`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `W88-yes.com`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#5b618e`, // Used for Offline Manifest
     themeColor: `#5b618e`, // Used for Offline Manifest
