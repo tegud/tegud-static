2e5d27981b3e39aa39c5fdcb5f4432e202b8df0d
vndangky <dinhvinh1710@gmail.com>
2019-06-04 21:48:48 +0700

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -33,10 +33,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
         <div class="nav-bar title-font">
           <div class="nav-bar-inner">
             <ul class="social-buttons">
-              <li><a href="https://bit.ly/w88vietnamfc" target="_blank" class="fa fa-usd-circle"></a></li>
-              <li><a href="https://www.w88wasia.com/default.aspx?affiliateid=42353" target="_blank" class="fa fa-usd-circle"></a></li>
-              <li><a href="https://affiliate.w88wap.com/Track.aspx?affiliateid=42353&language=vn" target="_blank" class="fa fa-trophy-alt"></a></li>
-            </ul>
+              <li><a href="https://bit.ly/w88vietnamfc" target="_blank" class="fa fa-twitter-square"></a></li>
+        </ul>
             <a href="https://www.w88-yes.com"><h1>Nhận 300k cược thử👉</h1></a>
           </div>
         </div>
