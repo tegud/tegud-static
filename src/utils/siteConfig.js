64f7287616876dd5846e18ff879b2844aee7d61e
vndangky <dinhvinh1710@gmail.com>
2019-06-04 20:56:58 +0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
     siteUrl: `https://www.w88-yes.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 10, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 5, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     shareImage: `/images/Ghost-Docs.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `W88 bonus --->`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `W88-yes.com`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#5b618e`, // Used for Offline Manifest
-    themeColor: `#01579b`, // Used for Offline Manifest
+    themeColor: `#5b618e`, // Used for Offline Manifest
 }
